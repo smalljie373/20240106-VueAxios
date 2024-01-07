3e9e07154ee03f18fe6a2a46ebed028638386d97
smalljie <i890505@gmail.com>
2024-01-07 19:57:09 +0800

    'axios'

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ### 使用的套件:
 * bootstrap v5.3.2
 * Vue 3
+* axios v1.1.2
+* sweetalert2 v11.10.2
--- js/index.js
+++ js/index.js
@@ -1,10 +1,37 @@
-const { createApp, ref} = Vue;
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import Swal from 'sweetalert2';
+const apiUrl = 'https://ec-course-api.hexschool.io/v2/admin/signin';
 
 createApp({
     setup() {
-        const message = ref('登出');
+        const loginData = ref({
+            username: '',
+            password: '',
+        });
+        const loginSubmit = async () => {
+            try{
+                const res = await axios.post(apiUrl,loginData.value);
+                Swal.fire({
+                    title: '登入成功',
+                    text: '3秒後將自動轉到產品列表頁面',
+                    icon: 'success',
+                    showConfirmButton: false,
+                    timer: 3000
+                });
+                setTimeout(() => {
+                    console.log(res);
+                },3000)
+            }catch(error){
+                console.log(error);
+            }
+            loginData.value={
+                username: '',
+                password: '',
+            }
+        }
         return {
-            message
+            loginData,
+            loginSubmit
         }
     }
 }).mount('#app');
